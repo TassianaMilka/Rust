fe918b93b014bab6580f2cfa0103e0ec7299cb20
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-11-13 20:54:49 -0300

    Create Rustexample2.rs

---
+++ Rust/Rustexample2/Rustexample2.rs
@@ -0,0 +1,15 @@
+//Print one string example in code.
+
+fn main() {
+
+    //Variable string 
+    
+
+    let write="Learning Programming Language Rust basic example.";
+   
+    //Print string
+    
+    println!("{}",write);
+    
+ 
+}
