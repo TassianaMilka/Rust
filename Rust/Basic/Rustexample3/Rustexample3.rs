6151d5f37ae71cc143db9136a63402d547d35e29
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-11-13 21:25:13 -0300

    Create Rustexample3.rs

---
+++ Rust/Basic/Rustexample3/Rustexample3.rs
@@ -0,0 +1,17 @@
+// Create one company American 1200 with add 900 and return result.
+
+fn main() {
+
+    //Variable string and U$ money result
+    
+
+    let write="Company example money day U$.";
+    let result=12000+900;
+   
+    //Print string and result
+    
+    println!("{}",write);
+    println!("{}",result);
+    
+ 
+}
