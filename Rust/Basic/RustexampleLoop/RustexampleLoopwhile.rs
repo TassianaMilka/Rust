28372711ea2cdc36eae33a5be3dbc1da2e7ded37
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2026-01-26 13:57:48 -0300

    Create RustexampleLoopwhile.rs

---
+++ Rust/Basic/RustexampleLoop/RustexampleLoopwhile.rs
@@ -0,0 +1,23 @@
+//Code about loop while print message.
+
+fn main() {
+
+   //Variables 
+    
+   let count = 1;
+   let message=" I'm love Music Classic!";
+
+    
+     //Loop
+     while count <= 1 {
+    
+         
+      //Print
+      println!("{}",message);
+
+    //Stoop
+    break;
+    
+   }
+
+}
