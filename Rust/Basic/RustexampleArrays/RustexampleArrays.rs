cbd831f3ffbd1e4d2a6ea63cdcaddfcf2f29e893
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2026-01-26 14:04:56 -0300

    Create RustexampleArrays.rs

---
+++ Rust/Basic/RustexampleArrays/RustexampleArrays.rs
@@ -0,0 +1,12 @@
+//Create  about variable arrays string next print.
+
+fn main() {
+
+   //Variable Arrays string
+    
+   let op = ["Apple","Banana", "Potato", "Orange", "Tomato"];
+
+   //Print
+   println!("{}", op[4]);
+    
+}
