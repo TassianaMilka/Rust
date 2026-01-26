569b945862641c95f731ac51ea8d47ee5b8c23e8
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2026-01-26 13:31:45 -0300

    Add if-else structure for name validation
    Implement a basic if-else structure to validate a name string and print corresponding messages.

---
+++ Rust/Basic/RustexampleIfandelse/RustexampleIfandelse.rs
@@ -0,0 +1,24 @@
+//Create string with if and else print name or not name.
+
+fn main() {
+
+    //Variable string
+    let name="NameExample";
+
+    //Validation  structure string
+    if name=="NameExample" {
+
+    //Print
+    println!("True name!")
+
+    }else if name!="Name"{
+        //Print
+        println!("Name other!");
+    }
+
+    else {
+        //Print
+        println!("Erro code!")
+    }
+
+}
